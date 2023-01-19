89e7e7d0ebe289be66ccb38d6099d466da1919a6
Néstor <nestor_moreno098@hotmail.com>
2023-01-19 17:27:26 +0100

    Nivel 1 completado

---
+++ level1/my-project/src/components/Botons.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="button">
+        <button @click="$emit('botons', 1)">Anterior</button>
+        <button @click="$emit('botons', 2)">Següent</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BotonsItem',
+}
+</script>
+
+<style scoped>
+    button {
+        padding: 40px;
+        width: 50%;
+        font-size: 24px;
+    }
+</style>
---
+++ level1/my-project/src/components/Home.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="home">
+        <BotonsItem @botons="pasarFrase"></BotonsItem>
+        <EscenaItem :frases= "frasesArray" :currentSentence="currentSentence"></EscenaItem>
+    </div>
+
+</template>
+
+<script>
+import EscenaItem from './Escena.vue'
+import BotonsItem from './Botons.vue'
+
+export default {
+    name: 'HomeItem',
+    components: {
+       EscenaItem,
+       BotonsItem
+    },
+    data() {
+        return {
+           frasesArray: ["El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial", 
+           
+           "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.", 
+           
+           "L'heroi va decidir travessar la porta que el portava a casa", 
+           
+           "Mentrestant, altres herois no van tenir tanta sort en la seva elecció ..."],
+           currentSentence: 0,
+           next: true,
+           back: false
+        }
+    },
+    methods: {
+        pasarFrase(n) {
+            if(this.currentSentence == 1) {
+                this.back = true;
+            } else if(this.currentSentence == 0) {
+                this.back = false;
+                this.next = true;
+            }
+
+            if(n == 1 && this.back == true ) {
+                this.currentSentence--;
+            }
+             
+            if(n == 2 && this.next == true) {
+                this.currentSentence++
+            } 
+            
+            if(this.currentSentence == 3) {
+                this.next = false;
+                this.back = true;
+            } 
+            
+            console.log(this.currentSentence);
+            console.log("Next: " + this.next);
+            console.log("Back: " + this.back);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
